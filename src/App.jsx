39d43304870ac5fc2cd518207674feee4d1b0653
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-26 18:04:21 +0200

    feat: ✨ New experience section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero'
 import LogoSection from './sections/LogoSection'
 import NavBar from './components/NavBar'
 import ShowcaseSection from './sections/ShowcaseSection'
+import Experience from './sections/Experience'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <ShowcaseSection />
       <LogoSection />
       <FeatureCards />
+      <Experience />
     </>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -148,37 +148,37 @@ const techStackIcons = [
 const expCards = [
   {
     review:
-      'Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.',
-    imgPath: '/images/exp1.png',
+      'Ivan brought value and technical expertise to the team, significantly improving the performance of our Technical IT Support team.',
+    imgPath: '/images/exp1.svg',
     logoPath: '/images/logo1.png',
-    title: 'Frontend Developer',
-    date: 'January 2023 - Present',
+    title: 'Jr Full-Stack Developer',
+    date: 'September 2022 - April 2024',
     responsibilities: [
-      'Developed and maintained user-facing features for the Hostinger website.',
-      'Collaborated closely with UI/UX designers to ensure seamless user experiences.',
-      'Optimized web applications for maximum speed and scalability.'
+      'As the person in charge of the technical support system, I review errors and handle contact with customer.',
+      'Fix reported bugs if they are in my hand, Jira tasks management, GitHub releases and versioning.',
+      'Support Developers with basic tasks, create technical documentation, testing, API endpoints.'
     ]
   },
   {
     review:
-      "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: '/images/exp2.png',
-    logoPath: '/images/logo2.png',
-    title: 'Full Stack Developer',
-    date: 'June 2020 - December 2023',
+      "Ivan's work to Burger King's restaurant have been outstanding. He started working at the bar, taking orders and taking charge of the dining room. He continued with different responsibilities, such as opening and closing the restaurant. And he ended up preparing the orders in the kitchen and teaching the new employees.",
+    imgPath: '/images/exp2.webp',
+    logoPath: '/images/exp2.webp',
+    title: 'Waiter and Cook positions',
+    date: 'March 2018 - August 2022',
     responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
-      'Worked with backend engineers to integrate APIs seamlessly with the frontend.',
-      'Contributed to open-source projects that were used with the Docker ecosystem.'
+      'Started attending the bar, taking and preparing orders to clients.',
+      'Managing restaurant openings and closings.',
+      'Prepare kitchen orders and train new employees.'
     ]
   },
   {
     review:
-      'Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.',
+      'Ivan´s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.',
     imgPath: '/images/exp3.png',
     logoPath: '/images/logo3.png',
     title: 'React Native Developer',
-    date: 'March 2019 - May 2020',
+    date: 'March 2016 - February 2018',
     responsibilities: [
       "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
       'Improved app performance and user experience through code optimization and testing.',
@@ -194,7 +194,7 @@ const expLogos = [
   },
   {
     name: 'logo2',
-    imgPath: '/images/logo2.png'
+    imgPath: '/images/exp2.webp'
   },
   {
     name: 'logo3',
@@ -207,42 +207,42 @@ const testimonials = [
     name: 'Esther Howard',
     mentions: '@estherhoward',
     review:
-      'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+      'I can’t say enough good things about Ivan. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
     imgPath: '/images/client1.png'
   },
   {
     name: 'Wade Warren',
     mentions: '@wadewarren',
     review:
-      'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+      'Working with Ivan was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
     imgPath: '/images/client3.png'
   },
   {
     name: 'Guy Hawkins',
     mentions: '@guyhawkins',
     review:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Ivan was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Ivan's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ivan is the ideal partner.",
     imgPath: '/images/client2.png'
   },
   {
     name: 'Marvin McKinney',
     mentions: '@marvinmckinney',
     review:
-      'Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.',
+      'Ivan was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.',
     imgPath: '/images/client5.png'
   },
   {
     name: 'Floyd Miles',
     mentions: '@floydmiles',
     review:
-      'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!',
+      'Ivan’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!',
     imgPath: '/images/client4.png'
   },
   {
     name: 'Albert Flores',
     mentions: '@albertflores',
     review:
-      'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.',
+      'Ivan was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.',
     imgPath: '/images/client6.png'
   }
 ]
