3aa157c9d7b444d8643e6cc8da1ccd4876c083ee
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-26 12:55:53 +0200

    feat: ✨ Feature cards added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import LogoSection from './components/LogoSection'
-import NavBar from './components/NavBar'
+import FeatureCards from './sections/FeatureCards'
 import Hero from './sections/Hero'
+import LogoSection from './sections/LogoSection'
+import NavBar from './components/NavBar'
 import ShowcaseSection from './sections/ShowcaseSection'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Hero />
       <ShowcaseSection />
       <LogoSection />
+      <FeatureCards />
     </>
   )
 }
