ed7eabfcfa5b467a29da2070144e5bdd086616b2
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-27 22:48:30 +0200

    add: 🔨 Minor style change

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,6 +2,7 @@ import { useRef } from 'react'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import TitleHeader from '../components/TitleHeader'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -45,54 +46,64 @@ const ShowcaseSection = () => {
   }, [])
 
   return (
-    <section id='work' ref={sectionRef} className='app-showcase'>
-      <div className='w-full'>
-        <div className='showcaselayout'>
-          {/* LEFT */}
+    <section
+      id='projects'
+      ref={sectionRef}
+      className='app-showcase flex-center md:mt-40 mt-20 section-padding xl:px-0'
+    >
+      <div className='w-full h-full md:px-20 px-5'>
+        <TitleHeader title='Featured Projects' sub='🛠️ Check my expertise!' />
+        <div className='w-full mt-32'>
+          <div className='showcaselayout'>
+            {/* LEFT */}
 
-          <div ref={project1Ref} className='first-project-wrapper'>
-            <div className='image-wrapper'>
-              <img
-                src='/images/project1.webp'
-                alt='Minino Malo project Image'
-              />
-            </div>
-            <div className='text-content'>
-              <h2>Minino Malo - Ecommerce of handmade products</h2>
-              <p className='text-white-50 md:text-xl'>
-                Real Production Application builded with Next.js, TypeScript and
-                TailwindCSS for a fast, user-friendly experience.
-              </p>
-            </div>
-          </div>
-
-          {/* RIGHT */}
-          <div className='project-list-wrapper overflow-hidden'>
-            <div ref={project2Ref} className='project'>
-              <div className='image-wrapper bg-[#FCCCCD]'>
+            <div ref={project1Ref} className='first-project-wrapper'>
+              <div className='image-wrapper'>
                 <img
-                  src='/images/project2.webp'
-                  alt='File Driver Image project'
+                  src='/images/project1.webp'
+                  alt='Minino Malo project Image'
                 />
               </div>
-
-              <h2>
-                File Driver - Storage management and file sharing platform
-              </h2>
-              <p className='text-white-50 md:text-xl'>
-                Effortlessly upload, organize, and share files.
-              </p>
+              <div className='text-content'>
+                <h2>Minino Malo - Ecommerce of handmade products</h2>
+                <p className='text-white-50 md:text-xl'>
+                  Real Production Application builded with Next.js, TypeScript
+                  and TailwindCSS for a fast, user-friendly experience.
+                </p>
+              </div>
             </div>
 
-            <div ref={project3Ref} className='project'>
-              <div className='image-wrapper bg-[#A1BFF3]'>
-                <img src='/images/project3.webp' alt='Travely project Image' />
+            {/* RIGHT */}
+            <div className='project-list-wrapper overflow-hidden'>
+              <div ref={project2Ref} className='project'>
+                <div className='image-wrapper bg-[#FCCCCD]'>
+                  <img
+                    src='/images/project2.webp'
+                    alt='File Driver Image project'
+                  />
+                </div>
+
+                <h2>
+                  File Driver - Storage management and file sharing platform
+                </h2>
+                <p className='text-white-50 md:text-xl'>
+                  Effortlessly upload, organize, and share files.
+                </p>
               </div>
 
-              <h2>Travely - Travel Agency dashboard</h2>
-              <p className='text-white-50 md:text-xl'>
-                Create and find unique, customizable, AI-generated trips.
-              </p>
+              <div ref={project3Ref} className='project'>
+                <div className='image-wrapper bg-[#A1BFF3]'>
+                  <img
+                    src='/images/project3.webp'
+                    alt='Travely project Image'
+                  />
+                </div>
+
+                <h2>Travely - Travel Agency dashboard</h2>
+                <p className='text-white-50 md:text-xl'>
+                  Create and find unique, customizable, AI-generated trips.
+                </p>
+              </div>
             </div>
           </div>
         </div>
