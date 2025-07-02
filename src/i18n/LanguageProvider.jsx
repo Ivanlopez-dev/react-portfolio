f8a4ef59fcfc2cf1179305c9395f0897aa9478b1
Ivan López Rodríguez <ivaan100@gmail.com>
2025-07-02 12:39:53 +0200

    feat: 🌐 Save lang switched into localstorage

--- src/i18n/LanguageProvider.jsx
+++ src/i18n/LanguageProvider.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IntlProvider } from 'react-intl'
+
 import messagesEn from './en.json'
 import messagesEs from './es.json'
 
@@ -9,6 +10,11 @@ const messages = {
 }
 
 const getInitialLocale = () => {
+  // If user's switch Language, Save it on localStorage
+  const savedLocale = localStorage.getItem('locale')
+  if (savedLocale && ['en', 'es'].includes(savedLocale)) return savedLocale
+
+  // Set Origin language based on user's Browser language
   const browserLang = navigator.language || navigator.userLanguage
   return browserLang.toLowerCase().startsWith('es') ? 'es' : 'en'
 }
@@ -18,7 +24,15 @@ const LanguageContext = React.createContext()
 const LanguageProvider = ({ children }) => {
   const [locale, setLocale] = useState(getInitialLocale())
 
-  const switchLanguage = lang => setLocale(lang)
+  const switchLanguage = lang => {
+    setLocale(lang)
+    localStorage.setItem('locale', lang)
+  }
+
+  useEffect(() => {
+    // Ensure that locale in state is always reflected in localStorage
+    localStorage.setItem('locale', locale)
+  }, [locale])
 
   return (
     <LanguageContext.Provider value={{ locale, switchLanguage }}>
